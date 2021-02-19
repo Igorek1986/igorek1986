5b3d5a6780909748155a1374735a0822a4ad8043
Igorek1986 <51848581+Igorek1986@users.noreply.github.com>
2021-02-19 19:01:40 +0500

    Add files via upload

---
+++ zmp.sh
@@ -0,0 +1,3 @@
+/home/igor/zmp/zmp-linux-amd64 --host 192.168.88.14 --best "https://pastebin.com/raw/Lm41DLMs"
+#/home/igor/zmp/zmp-linux-amd64 --host 192.168.88.14 --best "http://immo.date/ero.m3u"
+#/home/igor/zmp/zmp-linux-amd64 --host 192.168.88.14 --best "https://mediacenter.fun/Z-MediaProxy-ZabavaFILM.Karnei4.21.12.2020.m3u8"
